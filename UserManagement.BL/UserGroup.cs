eee959796c6cc6770f209fc2b96235b7f49114ea
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-13 19:45:08 +0200

    Add implmentation for UserGroup class: getters, setters and constructors

--- UserManagement.BL/UserGroup.cs
+++ UserManagement.BL/UserGroup.cs
@@ -1,7 +1,16 @@
-﻿namespace UserManagement.BL
+﻿using System.Collections.Generic;
+
+namespace UserManagement.BL
 {
     public class UserGroup
     {
-        
+        public string GroupName { get; private set; }
+        public List<string> UsersInGroup { get; private set; }
+
+        public UserGroup(string groupName)
+        {
+            GroupName = groupName;
+            UsersInGroup = new List<string>();
+        }
     }
 }
