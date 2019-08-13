d4429480410afbe336a580ad7ba29dd432c9e88c
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-13 19:41:33 +0200

    Implement getters, setters and constructors for User class

--- UserManagement.BL/User.cs
+++ UserManagement.BL/User.cs
@@ -1,7 +1,35 @@
-﻿namespace UserManagement.BL
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserManagement.BL
 {
     public class User
     {
-        
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<string> UserGroup { get; private set; }
+
+        public User(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+            UserGroup = new List<string>();
+        }
+
+        public User(string login,
+                    string password,
+                    string firstName,
+                    string lastName,
+                    DateTime birthDate,
+                    List<string> userGroup) : this(login, password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            UserGroup = userGroup;
+        }
     }
 }
