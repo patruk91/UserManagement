9676ffd201f32a46dcbabdfea7e4ced18ea5b8f1
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-15 12:44:09 +0200

    Implement methods to get: not empty string and number from string

--- UserManagement.VL/Read.cs
+++ UserManagement.VL/Read.cs
@@ -10,5 +10,39 @@ namespace UserManagement.VL
         {
             _view = view;
         }
+
+        private string GetInput()
+        {
+            return Console.ReadLine();
+        }
+
+        public string GetNotEmptyString()
+        {
+            string input = "";
+            while (string.IsNullOrWhiteSpace(input))
+            {
+                input = GetInput();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    _view.DisplayError("Please, provide not empty data");
+                }
+            }
+            return input;
+        }
+
+        public int GetNumberFromString()
+        {
+            string stringNumber = "";
+            int number;
+            while (!int.TryParse(stringNumber, out number))
+            {
+                stringNumber = GetNotEmptyString();
+                if(!int.TryParse(stringNumber, out number))
+                {
+                    _view.DisplayError("Please, provide numeric data");
+                }
+            }
+            return number;
+        }
     }
 }
