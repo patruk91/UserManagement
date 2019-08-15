a56296a6c09382dc8905795f9d35a00ee9f7950f
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-15 12:45:06 +0200

    Display: question, error, action request, users

--- UserManagement.VL/View.cs
+++ UserManagement.VL/View.cs
@@ -1,7 +1,33 @@
-﻿namespace UserManagement.VL
+﻿using System;
+using System.Collections.Generic;
+using UserManagement.M;
+
+namespace UserManagement.VL
 {
     public class View
     {
-        
+        public void DisplayMenu(string menu)
+        {
+            Console.WriteLine(menu);
+        }
+
+        public void DisplayError(string error)
+        {
+            Console.WriteLine($"ERROR: {error}!");
+        }
+
+        public void DisplayActionRequest(string message)
+        {
+            Console.Write($"{message}: ");
+
+        }
+
+        public void DisplayUsers(List<User> users)
+        {
+            foreach (User user in users)
+            {
+                Console.WriteLine(user);
+            }
+        }
     }
 }
